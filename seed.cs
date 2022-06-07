ad4f17cf3ef9a15c89a4902ffc684f94c4bfb73a
esmaeel motaghi <esmaeel.m@live.com>
2022-06-07 21:37:00 +0430

    idk

--- seed.cs
+++ seed.cs
@@ -16,7 +16,10 @@ namespace BackHost
             //var test =await _context.Products.Where(c => c.Labels.Any(d => d == 1)).ToListAsync();
             var permissiondb = await AccDB.Permissions.FirstOrDefaultAsync(c => c.Title == "DB");
             var permissionCity = await AccDB.Permissions.FirstOrDefaultAsync(c => c.Title == "DB/City");
+            var permissionSellMethod = await AccDB.Permissions.FirstOrDefaultAsync(c => c.Title == "DB/SellMethod");
+            var permissionOrganization = await AccDB.Permissions.FirstOrDefaultAsync(c => c.Title == "DB/Organization");
             var permissionVahed = await AccDB.Permissions.FirstOrDefaultAsync(c => c.Title == "DB/Vahed");
+            var permissionAccountManager = await AccDB.Permissions.FirstOrDefaultAsync(c => c.Title == "AccountManager");
             var e = await _context.Cities.AnyAsync(c => c.Id > 0);
             if (e) return false;
             foreach (var item in new string[] {
@@ -50,15 +53,43 @@ namespace BackHost
                 };
                 var role = new BaseApplicationRole() { Color = "#ff0000", Create = DateTime.Now, Title = item, Name = item.Split("_")[1] };
                 await roleManager.CreateAsync(role);
-                await userManager.CreateAsync(user, "123456789");
-                await userManager.AddToRoleAsync(user, item.Split("_")[1]);
+                if(item.Contains("ilam"))
+                {
+                    await userManager.CreateAsync(user, "123456789");
+                    await userManager.AddToRoleAsync(user, item.Split("_")[1]);
+                    await userManager.AddToRoleAsync(user, "NormalUser");
+                    for (int i = 2; i < 7; i++)
+                    {
+                        var user2 = new BaseApplicationUser()
+                        {
+                            Avatar = "",
+                            Create = DateTime.Now,
+                            UserName = item.Split("_")[1] + i.ToString(),
+                            FirstName = "مدیر",
+                            LastName = item.Split("_")[0],
+                            EmailConfirmed = true,
+                            Status = Statuses.Active,
+                            Email = item.Split("_")[1] + "@gmail.com"
+                        };
+                        await userManager.CreateAsync(user2, "123456789");
+                        await userManager.AddToRoleAsync(user2, item.Split("_")[1]);
+                        await userManager.AddToRoleAsync(user2, "NormalUser");
+                    }
+                }
+                else
+                {
+                    await userManager.CreateAsync(user, "123456789");
+                    await userManager.AddToRoleAsync(user, item.Split("_")[1]);
+                    await userManager.AddToRoleAsync(user, "NormalUser");
+                }
+
                 var p = new RolePermission()
                 {
                     EntityIds = new List<long> { city.Id },
                     PermissionId = permissionCity.Id,
                     RoleId = role.Id,
                     Title = permissionCity.Title,
-                    Access=new List<string> { "Get"}
+                    Access = new List<string> { "Get" }
                 };
                 AccDB.RolePermissions.Add(p);
                 var p2 = new RolePermission()
@@ -79,14 +110,29 @@ namespace BackHost
                     Access = new List<string> { "Export", "Delete", "Set", "Get", "UploadAsync", "GetSingle", "View" }
                 };
                 AccDB.RolePermissions.Add(p3);
+                var p4 = new RolePermission()
+                {
+                    EntityIds = new List<long> { },
+                    PermissionId = permissionOrganization.Id,
+                    RoleId = role.Id,
+                    Title = permissionOrganization.Title,
+                    Access = new List<string> { "Get" }
+                };
+                AccDB.RolePermissions.Add(p4);
+                var p5 = new RolePermission()
+                {
+                    EntityIds = new List<long> { },
+                    PermissionId = permissionSellMethod.Id,
+                    RoleId = role.Id,
+                    Title = permissionSellMethod.Title,
+                    Access = new List<string> { "Get" }
+                };
+                AccDB.RolePermissions.Add(p5);
                 await _context.SaveChangesAsync();
                 await AccDB.SaveChangesAsync();
             }
             var cc = await _context.Cities.FirstOrDefaultAsync(c => c.Title == "");
-            for (int i = 2; i < 7; i++)
-            {
 
-            }
             //foreach (var item in new string[] { "ايلام", "ايوان", "آبدانان", "دره شهر", "دهلران" })
             //{
             //    _context.Add(new City { Title = item });
